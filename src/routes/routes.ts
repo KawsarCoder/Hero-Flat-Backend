bff57a45e65d5bdb15314846a5dc5b5b553acae1
KawsarCoder <md.kawsar.developer@gmail.com>
2024-06-05 12:36:20 +0600

    routes added

---
+++ src/routes/routes.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import bookingRoutes from "../app/modules/Bookings/booking.route";
+import flatRoutes from "../app/modules/Flat/flat.route";
+import profileRoutes from "../app/modules/Profile/profile.route";
+import authRoutes from "../app/modules/auth/auth.route";
+import metaRoutes from "../app/modules/meta/meta.route";
+import userRoutes from "../app/modules/users/user.route";
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: "/users",
+    route: userRoutes,
+  },
+  {
+    path: "/auth",
+    route: authRoutes,
+  },
+  {
+    path: "/flats",
+    route: flatRoutes,
+  },
+  {
+    path: "/meta",
+    route: metaRoutes,
+  },
+  {
+    path: "/bookings",
+    route: bookingRoutes,
+  },
+  {
+    path: "/profile",
+    route: profileRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
